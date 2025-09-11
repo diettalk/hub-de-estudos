7dfa2765b945a5d0473cb9def999e6d1845f4df5
Hélio Tavares <diettallk@gmail.com>
2025-09-11 10:51:12 -0300

    fix(editor): refatora WikiLink para corrigir bug de clique e foco

--- src/components/WikiLink.ts
+++ src/components/WikiLink.ts
@@ -1,7 +1,5 @@
-import { Mark } from '@tiptap/core';
+import { Mark, mergeAttributes } from '@tiptap/core';
 
-// Define um novo tipo de "Mark" para os nossos links [[...]]
-// para que possamos estilizá-los de forma diferente de links normais.
 export const WikiLink = Mark.create({
   name: 'wikiLink',
   
@@ -17,8 +15,8 @@ export const WikiLink = Mark.create({
   },
 
   renderHTML({ HTMLAttributes }) {
-    // Adiciona uma classe CSS específica para podermos estilizar no TextEditor.css
-    return ['a', { ...HTMLAttributes, class: 'wiki-link' }, 0];
+    // Adiciona uma classe CSS que será usada no TextEditor.tsx
+    return ['a', mergeAttributes({ class: 'wiki-link' }, HTMLAttributes), 0];
   },
 });
 
